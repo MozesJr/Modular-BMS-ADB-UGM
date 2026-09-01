4dc6c9f9550f248d2a94cbe5345a12f7493a9e9e
Mozes <mozessapari@gmail.com>
2026-09-01 14:09:37 +0700

    Feat: Update code untuk DockerFIle

--- FE/next.config.ts
+++ FE/next.config.ts
@@ -4,6 +4,8 @@ import type { NextConfig } from "next";
 const BACKEND_URL = process.env.BACKEND_URL ?? "http://localhost:4000";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
